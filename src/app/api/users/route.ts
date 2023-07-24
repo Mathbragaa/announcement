62df34832129820625aac4f5bd8d3dabb146b5ef
Mathbragaa <mathbraga16@gmail.com>
2023-07-24 15:18:15 -0300

    api post users + password crypto

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,9 +1,34 @@
-
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import dbConnect from '@/utils/dbConnect'
- 
-export async function GET(request: Request) {
-    
-    await dbConnect()
-  return NextResponse.json({ success: true})
+import {crypto} from '@/utils/password'
+import UsersModel from '@/models/users'
+
+interface CreateUserRequest {
+  name: string
+  email: string
+  password: string
+  passwordCrypto: string
+}
+
+export async function GET(req: NextRequest) {
+  await dbConnect()
+  return NextResponse.json({ success: true })
+}
+
+export async function POST(req: NextRequest) {
+  await dbConnect()
+
+  const body = await req.text()
+  const { name, email, password }: CreateUserRequest = JSON.parse(body)
+
+  const passwordCrypto = await crypto(password)
+  
+  const user = new UsersModel({ name, email, password:passwordCrypto})
+
+  try {
+    await user.save()
+    return NextResponse.json({ status: 201, success: true })
+  } catch (error) {
+    return NextResponse.json({ status: 500, success: false })
+  }
 }
